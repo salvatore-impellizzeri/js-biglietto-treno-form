aa5a51f7d02aa653ed5aeba0c73620484ec2a552
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-07-12 20:35:34 +0200

    Realizzazione biglietto

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,52 @@
-// RICHIESTE
-
 const infoForm = document.querySelector('form');
 infoForm.addEventListener("submit", 
     function(event){
         console.log('event', event, typeof event);
         event.preventDefault();
 
-        const myName = document.getElementById("name");
-        console.log("Nome e Cognome:", myName.value, typeof myName.value);
+        const myName = document.getElementById("name").value;
+        console.log("Nome e Cognome:", myName, typeof myName);
 
-        const myDistance = document.getElementById("distance");
-        console.log("Distanza:", myDistance.value, "km" ,typeof myDistance.value);
+        const myDistance = document.getElementById("distance").value;
+        console.log("Distanza:", myDistance, "km" ,typeof myDistance);
     
         const myAge = document.getElementById("age").value;
         console.log("Fascia d'età:", age.value, typeof age.value);
 
-        const myTicket = document.getElementsByClassName("noneticket");
-
-        if (myTicket.classList.contains("noneticket")){
+        const myTicket = document.getElementById("my-ticket");
+        
+            if(myAge == ("minorenne" || "maggiorenne" || "over65")){
             myTicket.classList.replace('noneticket', 'ticket');
         }
 
         // PREZZO BASE
-        let pricetot = myDistance.value * 0.21;
-        console.log("pricetot:", pricetot.toFixed(2), "€");
+        let priceTot = myDistance * 0.21;
+        console.log("priceTot:", priceTot.toFixed(2), "€");
 
         // SCONTO
         if (myAge === "minorenne"){
-            pricetot = pricetot * 0.80;
+            priceTot = priceTot * 0.80;
+            document.getElementById("discount").innerHTML= "Offerta Minorenni";
         }
 
         else if (myAge === "over65"){
-            pricetot = pricetot * 0.60;
+            priceTot = priceTot * 0.60;
+            document.getElementById("discount").innerHTML= "Offerta Over 65";
+        }
+        
+        else{
+            document.getElementById("discount").innerHTML= "Offerta Standard";
         }
 
         // PREZZO FINALE
-        console.log("pricetot:", pricetot.toFixed(2), "€");
+        console.log("priceTot:", priceTot.toFixed(2), "€");
+
+        document.getElementById("passenger").innerHTML= myName;
+        document.getElementById("price").innerHTML= priceTot.toFixed(2) + "€";
+
+        const cpCode =  Math.floor(Math.random() * 100000);
+
+        document.getElementById("codiceCP").innerHTML = cpCode;
     }
 
 )
