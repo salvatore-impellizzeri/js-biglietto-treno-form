4fdbf9a3a5e75a8616a9ff3c930566cfa873ff1f
salvatore-impellizzeri <salvatoreimpellizzeri2001@gmail.com>
2024-07-12 18:03:36 +0200

    Completata la parte di java script

---
+++ js/scripts.js
@@ -0,0 +1,35 @@
+// RICHIESTE
+
+const infoForm = document.querySelector('form');
+infoForm.addEventListener("submit", 
+    function(event){
+        console.log('event', event, typeof event);
+        event.preventDefault();
+
+        const myName = document.getElementById("name");
+        console.log("Nome e Cognome:", myName.value, typeof myName.value);
+
+        const myDistance = document.getElementById("distance");
+        console.log("Distanza:", myDistance.value, "km" ,typeof myDistance.value);
+    
+        const myAge = document.getElementById("age").value;
+        console.log("Fascia d'età:", age.value, typeof age.value);
+
+        // PREZZO BASE
+        let pricetot = myDistance.value * 0.21;
+        console.log("pricetot:", pricetot.toFixed(2), "€");
+
+        // SCONTO
+        if (myAge === "minorenne"){
+            pricetot = pricetot * 0.80;
+        }
+
+        else if (myAge === "over65"){
+            pricetot = pricetot * 0.60;
+        }
+
+        // PREZZO FINALE
+        console.log("pricetot:", pricetot.toFixed(2), "€");
+    }
+
+)
